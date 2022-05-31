3f24a1edbfbeb42113a3bb9056fccf1d4164180c
FreddyGM20 <85004658+FreddyGM20@users.noreply.github.com>
2022-05-30 21:03:19 -0500

    Update registrar.php

--- php/src/registrar.php
+++ php/src/registrar.php
@@ -1,28 +1,13 @@
-?php 
-$ch = curl_init();
+<?php 
 
-curl_setopt($ch, CURLOPT_URL, 'http://localhost:8000/registro.php');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POST, 1);
-$post = array(
-    'Usuario' => 'FreddyGM',
-    'Contraseña' => '123456',
-    'NRC' => '1234'
-);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-
-$result = curl_exec($ch);
-if (curl_errno($ch)) {
-    echo 'Error:' . curl_error($ch);
-}
-curl_close($ch);
-$res[]=array($result);
-print_r($ch);
 $existe=0;
 include("connect.php");
-	    $Usuario = $ch[0];
-	    $Contraseña = $ch[1];
-	    $NRC = $ch[2];
+        echo "Escriba su nombre de usuario";
+	    $Usuario = Console.ReadLine();
+        echo "Escriba su contraseña";
+	    $Contraseña = Console.ReadLine();
+        echo "Escriba su NRC";
+	    $NRC = Console.ReadLine();
         $query = $conn->query("SELECT * FROM users");
         echo "Primera fase";
         if (!empty($query->num_rows) && $query->num_rows > 0){
@@ -76,5 +61,6 @@ include("connect.php");
                   echo "entra aqui error";
              } 
         }
+        
 
 ?>
